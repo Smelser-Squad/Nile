98d74bcd42000f6545aceaced1a6a156187fd017
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-04-12 10:37:55 -0400

    Initial Gradle project

---
+++ nile-backend/src/test/java/com/tp/Nile/NileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.Nile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
