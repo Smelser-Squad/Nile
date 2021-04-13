1e9f482ce122d860d8c362063fb4b039665d40aa
Charles Hall <charleshall@C02DFWLKML7H.local>
2021-04-13 10:54:19 -0400

    Added buttons and styling

--- nile-frontend/src/components/MoreProducts/MoreProducts.js
+++ nile-frontend/src/components/MoreProducts/MoreProducts.js
@@ -1,18 +1,30 @@
+import './MoreProducts.css';
+
 function MoreProducts() {
     return (
         <div class="container">
             <div class="header">
-                <h2>Customers also bought these products</h2>
+                <h3 id="customer-bought">Customers also bought these products</h3>
                 <p class="pages">Page 1 of 2</p>
             </div>
 
             <div class="left-nav">
+                <button class="nav-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                    </svg>
+                </button>
             </div>
             
             <div class="products-container">
             </div>
-
+            
             <div class="right-nav">
+                <button class="nav-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                </button>
             </div>
         </div>
     );
