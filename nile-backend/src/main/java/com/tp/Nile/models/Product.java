0880fc0df5715df9b44ee529fca4ce13d511542c
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-04-13 14:05:04 -0400

    Added Vendor model

--- nile-backend/src/main/java/com/tp/Nile/models/Product.java
+++ nile-backend/src/main/java/com/tp/Nile/models/Product.java
@@ -3,7 +3,11 @@ package com.tp.Nile.models;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,6 +16,10 @@ import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "product")
 public class Product implements Serializable {
@@ -21,17 +29,17 @@ public class Product implements Serializable {
     private Integer productId;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "fk_category_id", referencedColumnName = "category_id")
     @JsonBackReference
     private Category categoryId;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "vendor_id")
+    @JoinColumn(name = "fk_vendor_id", referencedColumnName = "vender_id")
     @JsonBackReference
     private Vendor vendorId;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "type_id")
+    @JoinColumn(name = "fk_type_id", referencedColumnName = "type_id")
     @JsonBackReference
     private Type type;
 
@@ -74,107 +82,5 @@ public class Product implements Serializable {
     @JsonManagedReference
     private Set<Question> questions = new HashSet<>();
 
-    public Product(Integer productId, Category categoryId, Vendor vendorId, Type type, Double price, String name, String description, String brand, Set<Photo> photos, Set<Feature> features, Set<Order> orders, Set<Question> questions) {
 
-        this.productId=productId;
-        this.categoryId = categoryId;
-        this.vendorId = vendorId;
-        this.type = type;
-        this.price = price;
-        this.name = name;
-        this.description = description;
-        this.brand = brand;
-        this.features = features;
-        this.orders = orders;
-        this.questions = questions;
-    }
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public Category getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Category categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Vendor getVendorId() {
-        return vendorId;
-    }
-
-    public void setVendorId(Vendor vendorId) {
-        this.vendorId = vendorId;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-
-    public Set<Feature> getFeatures() {
-        return features;
-    }
-
-    public void setFeatures(Set<Feature> features) {
-        this.features = features;
-    }
-
-    public Set<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(Set<Order> orders) {
-        this.orders = orders;
-    }
-
-    public Set<Question> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(Set<Question> questions) {
-        this.questions = questions;
-    }
 }
--- nile-backend/src/main/java/com/tp/Nile/models/Vendor.java
+++ nile-backend/src/main/java/com/tp/Nile/models/Vendor.java
@@ -1,4 +1,30 @@
 package com.tp.Nile.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "vendor")
 public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vendor_id")
+    @JsonBackReference
+    private Integer vendorId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+
 }
