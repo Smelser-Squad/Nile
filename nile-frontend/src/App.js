fda338351f8ba7b11ba6661f17c149ad58af820f
YesratRahman <yr2818@vt.edu>
2021-04-13 15:15:26 -0400

    Navigation and home component

--- nile-frontend/src/App.js
+++ nile-frontend/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import MoreProducts from './components/MoreProducts/MoreProducts';
 import Header from './components/Header/Header.js'; 
+import Home from './components/Home/Home.js';
 function App() {
     return ( 
         // eslint-disable-next-line
         <div className = "App" >
             <Header /> 
-           
-            {/* <MoreProducts /> */}
+            <Home /> 
         </div>
     );
 }
