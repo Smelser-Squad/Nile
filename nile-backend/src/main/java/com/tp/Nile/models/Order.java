bcc610e6f23cdd7676611c97a716b157cda904d1
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-04-13 15:44:14 -0400

    removed unnecessary getters/setters.

--- nile-backend/src/main/java/com/tp/Nile/models/Order.java
+++ nile-backend/src/main/java/com/tp/Nile/models/Order.java
@@ -30,50 +30,13 @@ public class Order implements Serializable {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(name="purchase_date", nullable = false)
+    @Column(name = "purchase_date", nullable = false)
     private LocalDate purchaseDate;
 
-    @Column(name="status", nullable = false)
+    @Column(name = "status", nullable = false)
     private String status;
 
     @OneToMany(mappedBy = "order")
     private Set<OrderProduct> orderProducts = new HashSet<>();
 
-    public Order(User user, LocalDate purchaseDate, String status) {
-        this.user = user;
-        this.purchaseDate = purchaseDate;
-        this.status = status;
-    }
-
-    public Integer getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Integer orderId) {
-        this.orderId = orderId;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDate getPurchaseDate() {
-        return purchaseDate;
-    }
-
-    public void setPurchaseDate(LocalDate purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
