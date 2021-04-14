bd2be8756dd48c8499f5942560405f0b6de4a7cc
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-04-14 13:05:33 -0400

    merging with master

--- nile-backend/src/main/java/com/tp/Nile/controllers/ReviewController.java
+++ nile-backend/src/main/java/com/tp/Nile/controllers/ReviewController.java
@@ -1,16 +1,70 @@
 package com.tp.Nile.controllers;
 
+import com.tp.Nile.exceptions.*;
 import com.tp.Nile.services.ReviewService;
+import com.tp.Nile.services.ReviewServiceImpl;
+import com.tp.Nile.models.Review;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
-    @Autowired
-    ReviewService service;
 
+    @Autowired
+    ReviewServiceImpl service;
+
+    @PostMapping("/addReview")
+    public ResponseEntity addReview(@RequestBody Review review) {
+        return ResponseEntity.ok(service.addReview(review));
+    }
+
+    @GetMapping("/reviews")
+    public ResponseEntity getReviews() {
+        return ResponseEntity.ok(service.getAllReviews());
+    }
+
+    @GetMapping("/review/{reviewId}")
+    public ResponseEntity getReviewById(@PathVariable Integer reviewId) {
+        try {
+            return ResponseEntity.ok(service.getReviewById(reviewId));
+        } catch (NullReviewIdException | InvalidReviewIdException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/reviews/{userId}")
+    public ResponseEntity getReviewsByUserId(@PathVariable Integer userId) {
+        try {
+            return ResponseEntity.ok(service.getReviewsByUserId(userId));
+        } catch (NullUserIdException | InvalidUserIdException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/updateReview")
+    public ResponseEntity updateReview(@RequestBody Review review) {
+        return ResponseEntity.ok(service.updateReview(review));
+    }
+
+    @DeleteMapping("/deleteReview/{reviewId}")
+    public String deleteReview(@PathVariable Integer reviewId) {
+        {
+            String toReturn="";
+            try {
+                if (service.deleteReview(reviewId)) {
+                    toReturn ="Product " + reviewId + "deleted";
+                }else{
+                    toReturn="Product " + reviewId + "not found";
+                }
+            }catch (InvalidReviewIdException | NullReviewIdException ex){
+                ex.getMessage();
+            }
+            return toReturn;
+        }
+    }
 }
+
+
