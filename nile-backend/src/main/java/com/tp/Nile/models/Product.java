3f715b014bfbcd747ae29872d9d00b0841372170
StephenDMay <71042502+StephenDMay@users.noreply.github.com>
2021-04-14 10:20:21 -0400

    Product photos frontend (#53)
    * created product photo model
    
    * added lombok
    
    * added color column for color picker
    
    Co-authored-by: Charles Hall <77343953+charleshall1998@users.noreply.github.com>
    Co-authored-by: wcollins37 <collinswilliam122396@gmail.com>

--- nile-backend/src/main/java/com/tp/Nile/models/Product.java
+++ nile-backend/src/main/java/com/tp/Nile/models/Product.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -70,8 +71,10 @@ public class Product implements Serializable {
     @JsonManagedReference
     private Set<Order> orders = new HashSet<>();
 
+
     @OneToMany(mappedBy = "product")
     private Set<OrderProduct> orderProducts = new HashSet<>();
+
 
     @OneToMany(mappedBy = "product")
     private Set<ProductFeature> productFeatures = new HashSet<>();
