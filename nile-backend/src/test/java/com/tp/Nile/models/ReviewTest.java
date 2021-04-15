a96b1ade25497f536f028b9d412f57a88d35e13d
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-04-15 15:16:15 -0400

    pushing front end changes

--- nile-backend/src/test/java/com/tp/Nile/models/ReviewTest.java
+++ nile-backend/src/test/java/com/tp/Nile/models/ReviewTest.java
@@ -1,102 +1,102 @@
-package com.tp.Nile.models;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import com.tp.Nile.repositories.ReviewRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-public class ReviewTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
-    private ReviewRepository repo;
-
-    @BeforeEach
-    public void setup() {
-        Review setupReview = new Review();
-        setupReview.setReviewId(8);
-        setupReview.setReviewDate(LocalDate.of(2020, 4, 14));
-        setupReview.setTitle("This is a test title");
-        setupReview.setSummary("This is a test summary");
-        setupReview.setUser(new User(100, null, null));
-        setupReview.setRating(4);
-
-        repo.save(setupReview);
-    }
-
-    @Test
-    @Rollback(false)
-    public void testCreateReview() {
-        Review newReview = new Review();
-        newReview.setReviewId(2);
-        newReview.setReviewDate(LocalDate.of(2020, 4, 14));
-        newReview.setTitle("This is a test title");
-        newReview.setSummary("This is a test summary");
-        newReview.setUser(new User(100, null, null));
-        newReview.setRating(3);
-
-        Review savedReview = repo.save(newReview);
-
-        assertNotNull(savedReview);
-    }
-
+//package com.tp.Nile.models;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import com.tp.Nile.repositories.ReviewRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.time.LocalDate;
+//import java.util.Optional;
+//
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = Replace.NONE)
+//public class ReviewTest {
+//
+//    @Autowired
+//    private TestEntityManager entityManager;
+//
+//    @Autowired
+//    private ReviewRepository repo;
+//
+//    @BeforeEach
+//    public void setup() {
+//        Review setupReview = new Review();
+//        setupReview.setReviewId(8);
+//        setupReview.setReviewDate(LocalDate.of(2020, 4, 14));
+//        setupReview.setTitle("This is a test title");
+//        setupReview.setSummary("This is a test summary");
+//        setupReview.setUser(new User(100, null, null));
+//        setupReview.setRating(4);
+//
+//        repo.save(setupReview);
+//    }
+//
 //    @Test
-//    public void findReviewById() {
+//    @Rollback(false)
+//    public void testCreateReview() {
+//        Review newReview = new Review();
+//        newReview.setReviewId(2);
+//        newReview.setReviewDate(LocalDate.of(2020, 4, 14));
+//        newReview.setTitle("This is a test title");
+//        newReview.setSummary("This is a test summary");
+//        newReview.setUser(new User(100, null, null));
+//        newReview.setRating(3);
 //
-//        int id = 8;
-//        LocalDate testDate = LocalDate.of(2020, 4, 14);
-//        String testTitle = "This is a test title";
-//        String testSummary = "This is a test summary";
-//        int userId = 100;
-//        int rating = 4;
+//        Review savedReview = repo.save(newReview);
 //
-//        Review testReview = repo.findById(id).isPresent() ?
-//                repo.findById(id).get(): null;
+//        assertNotNull(savedReview);
+//    }
 //
-//        assertEquals(testReview.getReviewId(), id);
+////    @Test
+////    public void findReviewById() {
+////
+////        int id = 8;
+////        LocalDate testDate = LocalDate.of(2020, 4, 14);
+////        String testTitle = "This is a test title";
+////        String testSummary = "This is a test summary";
+////        int userId = 100;
+////        int rating = 4;
+////
+////        Review testReview = repo.findById(id).isPresent() ?
+////                repo.findById(id).get(): null;
+////
+////        assertEquals(testReview.getReviewId(), id);
+////
+////        assertEquals(testReview.getTitle(), testTitle);
+////
+////        assertEquals(testReview.getSummary(), testSummary);
+////
+////        assertEquals(testReview.getUser().getUserId(), userId);
+////
+////        assertEquals(testReview.getRating(), rating);
+////
+////    }
 //
-//        assertEquals(testReview.getTitle(), testTitle);
-//
-//        assertEquals(testReview.getSummary(), testSummary);
-//
-//        assertEquals(testReview.getUser().getUserId(), userId);
-//
-//        assertEquals(testReview.getRating(), rating);
+//    @Test
+//    public void findReviewByUser() {
 //
 //    }
-
-    @Test
-    public void findReviewByUser() {
-
-    }
-
-    @Test
-    public void deleteReview() {
-
-    }
-
-    @Test
-    public void findAllReviews() {
-
-    }
-
-
-
-
-}
+//
+//    @Test
+//    public void deleteReview() {
+//
+//    }
+//
+//    @Test
+//    public void findAllReviews() {
+//
+//    }
+//
+//
+//
+//
+//}
--- nile-backend/src/test/java/com/tp/Nile/services/ReviewServiceImplTests.java
+++ nile-backend/src/test/java/com/tp/Nile/services/ReviewServiceImplTests.java
@@ -1,101 +1,101 @@
-//package com.tp.Nile.services;
-//
-//import com.tp.Nile.exceptions.InvalidReviewIdException;
-//import com.tp.Nile.exceptions.NullReviewAttributeException;
-//import com.tp.Nile.exceptions.NullReviewIdException;
-//import com.tp.Nile.models.Product;
-//import com.tp.Nile.models.Review;
-//import com.tp.Nile.models.Type;
-//import com.tp.Nile.models.User;
-//import org.junit.Before;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.*;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(SpringExtension.class)
-//@DataJpaTest
-//public class ReviewServiceImplTests {
-//
-//    @Autowired
-//    ReviewServiceImpl service;
-//
-//    @Test
-//    public void addReviewNullIdAttribute() {
-//
-//        Review test = new Review();
-//        test.setReviewId(null);
-//        test.setRating(4);
-//        test.setReviewDate(LocalDate.of(2020, 4, 14));
-//        test.setTitle("Title");
-//        test.setSummary("Summary");
-//        test.setUser(new User());
-//        try {
-//            service.addReview(test);
-//            fail();
-//        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
-//            ex.getMessage();
-//            fail();
-//        }
-//    }
-//
-//    @Test
-//    public void addReviewNonNullableAttribute() {
-//
-//        Review newTest = new Review();
-//        newTest.setReviewId(5);
-//        newTest.setRating(null);
-//        newTest.setReviewDate(LocalDate.of(2020, 4, 14));
-//        newTest.setTitle("Title");
-//        newTest.setSummary("Summary");
-//        newTest.setUser(new User());
-//        try {
-//            service.addReview(newTest);
-//            fail();
-//        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
-//            ex.getMessage();
-//            fail();
-//        }
-//
-//    }
-//
-//    @Test
-//    public void addReviewGoldenPath() throws NullReviewAttributeException, NullReviewIdException, InvalidReviewIdException {
-//        Review newTest = new Review();
-//        newTest.setReviewId(5);
-//        newTest.setRating(5);
-//        newTest.setReviewDate(LocalDate.of(2020, 4, 14));
-//        newTest.setTitle("Title");
-//        newTest.setSummary("Summary");
-//        newTest.setUser(new User());
-//        try {
-//            service.addReview(newTest);
-//            System.out.println(service.getReviewById(5).getReviewId());
-//
-//        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
-//            ex.getMessage();
-//            fail();
-//        }
-//
-//        assertEquals(5, service.getReviewById(5).getReviewId());
-//        assertEquals(LocalDate.of(2020,4,14), service.getReviewById(5));
-//        assertEquals("Summary", service.getReviewById(5).getSummary());
-//        assertEquals(5, service.getReviewById(5).getRating());
-//        assertEquals("Title", service.getReviewById(5).getTitle());
-//    }
-//
-//}
+package com.tp.Nile.services;
+
+import com.tp.Nile.exceptions.InvalidReviewIdException;
+import com.tp.Nile.exceptions.NullReviewAttributeException;
+import com.tp.Nile.exceptions.NullReviewIdException;
+import com.tp.Nile.models.Product;
+import com.tp.Nile.models.Review;
+import com.tp.Nile.models.Type;
+import com.tp.Nile.models.User;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class ReviewServiceImplTests {
+
+    @Autowired
+    ReviewServiceImpl service;
+
+    @Test
+    public void addReviewNullIdAttribute() {
+
+        Review test = new Review();
+        test.setReviewId(null);
+        test.setRating(4);
+        test.setReviewDate(LocalDate.of(2020, 4, 14));
+        test.setTitle("Title");
+        test.setSummary("Summary");
+        test.setUser(new User());
+        try {
+            service.addReview(test);
+            fail();
+        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
+            ex.getMessage();
+            fail();
+        }
+    }
+
+    @Test
+    public void addReviewNonNullableAttribute() {
+
+        Review newTest = new Review();
+        newTest.setReviewId(5);
+        newTest.setRating(null);
+        newTest.setReviewDate(LocalDate.of(2020, 4, 14));
+        newTest.setTitle("Title");
+        newTest.setSummary("Summary");
+        newTest.setUser(new User());
+        try {
+            service.addReview(newTest);
+            fail();
+        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
+            ex.getMessage();
+            fail();
+        }
+
+    }
+
+    @Test
+    public void addReviewGoldenPath() throws NullReviewAttributeException, NullReviewIdException, InvalidReviewIdException {
+        Review newTest = new Review();
+        newTest.setReviewId(5);
+        newTest.setRating(5);
+        newTest.setReviewDate(LocalDate.of(2020, 4, 14));
+        newTest.setTitle("Title");
+        newTest.setSummary("Summary");
+        newTest.setUser(new User());
+        try {
+            service.addReview(newTest);
+            System.out.println(service.getReviewById(5).getReviewId());
+
+        } catch (NullReviewIdException | InvalidReviewIdException | NullReviewAttributeException ex) {
+            ex.getMessage();
+            fail();
+        }
+
+        assertEquals(5, service.getReviewById(5).getReviewId());
+        assertEquals(LocalDate.of(2020,4,14), service.getReviewById(5));
+        assertEquals("Summary", service.getReviewById(5).getSummary());
+        assertEquals(5, service.getReviewById(5).getRating());
+        assertEquals("Title", service.getReviewById(5).getTitle());
+    }
+
+}
