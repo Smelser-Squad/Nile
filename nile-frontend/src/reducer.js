d50172c6856c08965f4cd7fef9ce73fb408e8c90
YesratRahman <49998106+YesratRahman@users.noreply.github.com>
2021-04-15 14:52:08 -0400

    Home photo (#89)
    * Products are shown and styled in the front page
    
    * Add to cart button updates cart
    
    Co-authored-by: Jeffrey Mann <jwmann22@gmail.com>

--- nile-frontend/src/reducer.js
+++ nile-frontend/src/reducer.js
@@ -2,12 +2,16 @@ export const initialState = {
     cart: [],
 };
 
+export const getCartTotal = (cart) =>
+    cart?.reduce((amount, product) => product.price + amount, 0);
+
 const reducer = (state, action) => {
+    console.log(action);
     switch (action.type) {
-        case 'ADD_TO_CART':
+        case "ADD_TO_CART":
             return {
                 ...state,
-                cart: [...state.cart, action.item],
+                cart: [...state.cart, action.product],
             };
         default:
             return state;
