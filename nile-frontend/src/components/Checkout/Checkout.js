584df6280dc699d2cf6c4267e0815184621d54bd
YesratRahman <yr2818@vt.edu>
2021-04-16 13:29:45 -0400

    Showing added product to cart, remove product from cart and updates the total

--- nile-frontend/src/components/Checkout/Checkout.js
+++ nile-frontend/src/components/Checkout/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from '../Cart/Subtotal/Subtotal'
+import CartProduct from '../Cart/CartProduct/CartProduct'
+import { useStateValue } from '../../StateProvider'
 
 function Checkout() {
+    const [{ cart }] = useStateValue();
     return (
         <div className='checkout' >
             <div className="checkout_left">
@@ -12,8 +15,25 @@ function Checkout() {
                     Shopping Cart
                 </h2>
 
-                {/* TODO: show added items to the cart */}
-                {/* Product*/}
+                {/* <CartProduct
+
+                    productId="12321341"
+                    name="Gift for Mom"
+                    price={80.99}
+                    rating={4}
+                    image="https://i.etsystatic.com/5584756/r/il/7d61bd/2957120029/il_1588xN.2957120029_kefx.jpg"
+                /> */}
+
+                {cart.map(product => (
+                    <CartProduct
+                        productId={product.productId}
+                        name={product.name}
+                        image={product.image}
+                        price={product.price}
+                        rating={product.rating}
+                    />
+
+                ))}
 
             </div>
 
