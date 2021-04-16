63030a27490fdf2c745181a477423e10dff8df60
YesratRahman <yr2818@vt.edu>
2021-04-16 10:42:49 -0400

    change file order frontend

--- nile-frontend/src/components/Checkout/Checkout.js
+++ nile-frontend/src/components/Checkout/Checkout.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from '../Cart/Subtotal/Subtotal'
 
 function Checkout() {
     return (
         <div className='checkout' >
             <div className="checkout_left">
 
+
                 <h2 className="checkout_title">
                     Shopping Cart
+                </h2>
 
-            </h2>
+                {/* TODO: show added items to the cart */}
+                {/* Product*/}
+
             </div>
 
             <div className="checkout_right">
-                <h2> Subtotal</h2>
+                <Subtotal />
             </div>
         </div>
     )
 }
+
 
 export default Checkout;
