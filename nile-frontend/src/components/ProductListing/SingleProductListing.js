871a4a52fc5b91a11867c268adc7609ccafcb815
Stephen May <stephendouglasm@gmail.com>
2021-04-16 08:30:57 -0400

    added alternate photos

--- nile-frontend/src/components/ProductListing/SingleProductListing.js
+++ nile-frontend/src/components/ProductListing/SingleProductListing.js
@@ -6,8 +6,8 @@ import ProductPhotos from '../ProductPhotos/ProductPhotos.jsx';
 function SingleProductListing() {
     return(
         <div className="SingleProductListing">
-            <h2>PRODUCT NAME HERE</h2>
-            <h2>PRODUCT DESCRIPTION HERE</h2>
+            <h2>WORLD'S #1 T SHIRT!!!!!!</h2>
+            <h3>THIS T SHIRT IS THE BEST SHIRT EVER, NO REGRETS, BUY NOW, YOURS FOREVER, NO STAINS GUARANTEE OR YOUR MONEY BACK!</h3>
             <ProductPhotos/>
             <MoreProducts/>
 
