4b48882436d6a5d540ba5d4df95b47aad99e0026
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-04-16 13:47:34 -0400

    product controller integration tests

--- nile-backend/src/main/java/com/tp/Nile/controllers/requests/AddProductRequest.java
+++ nile-backend/src/main/java/com/tp/Nile/controllers/requests/AddProductRequest.java
@@ -1,15 +1,12 @@
 package com.tp.Nile.controllers.requests;
 
-import com.tp.Nile.models.ProductPhoto;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
 @Setter
 @Getter
 public class AddProductRequest {
-
     private Integer categoryId;
     private Integer typeId;
     private Integer orderId;
@@ -17,13 +14,10 @@ public class AddProductRequest {
     private Integer reviewId;
     private List<Integer> photoId;
     private List<Integer> featureId;
-
     private String name;
     private String description;
     private Double price;
     private String brand;
     private Integer stock;
     private boolean primeEligible;
-
-
 }
--- nile-backend/src/main/java/com/tp/Nile/models/ProductPhoto.java
+++ nile-backend/src/main/java/com/tp/Nile/models/ProductPhoto.java
@@ -1,10 +1,9 @@
 package com.tp.Nile.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
@@ -15,6 +14,7 @@ import lombok.*;
 @Entity
 @Table(name= "product_photo")
 public class ProductPhoto implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "photo_id")
@@ -23,7 +23,7 @@ public class ProductPhoto implements Serializable {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "fk_product_id", referencedColumnName = "product_id")
     @JsonIgnoreProperties(value = {"photoList"})
-
+    @JsonBackReference
     private Product product;
 
     @Column(name = "image_src", nullable = false)
@@ -31,6 +31,5 @@ public class ProductPhoto implements Serializable {
 
     @Column(name = "color", nullable = false)
     private String color;
-
 
 }
