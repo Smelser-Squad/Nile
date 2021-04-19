812fb4baedf05f37c78185110e3049ef614b37b6
Charles Hall <charleshall@C02DFWLKML7H.local>
2021-04-19 16:07:23 -0400

    Mapped data using axios to display on page.

--- nile-frontend/src/components/MoreProducts/MoreProducts.jsx
+++ nile-frontend/src/components/MoreProducts/MoreProducts.jsx
@@ -1,30 +1,37 @@
 import './MoreProducts.css';
 import Product from "./Product";
 import {Grid} from '@material-ui/core';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import {getProducts} from '../../service/ProductService'
 
 function MoreProducts() {
 
-    const [data, setData] = useState([]);
+    const [products, setProducts] = useState([]);
+    const AllProducts = [];
 
-    const getProduct=async()=>{
-        await axios.get('http://localhost:80/api/products')
-        .then(res=>{
-            const products = res.data;
-            if(res.status === 200){
-                setData(products)
-            };
-        })
-        
-    };
+    if(products.length === 0) {
+    getProducts().then((list)=>
+        {
+            list.map((item)=>
+            AllProducts.push(item)
+            );
+            const products = AllProducts.map((product)=>
+                <Product
+                    productId={product.productId}
+                    name={product.name}
+                    price={product.price}
+                    rating={4}
+                    avgRating={4}
+                    image= "https://target.scene7.com/is/image/Target/GUEST_f3404e1d-acae-49cb-a7f0-6ce4f2b82793?wid=488&hei=488&fmt=pjpeg"
+                    reviewCount={16} 
+                />
 
-    useEffect(()=>{
-            getProduct();
-        },
-        []
-    );
-    
+            );
+            setProducts(products);
+        }
+        );
+    }
+
     return (
         <div class="container">
             <hr/>
@@ -45,63 +52,10 @@ function MoreProducts() {
                         </svg>
                     </button>
                 </Grid>
-
-
-                {/* <Grid item md={2}>
-                    <Product
-                    productId="12321341"
-                    name="Awesome Sweat Pants"
-                    price={20.00}
-                    avgRating={4}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fsweatpants-png-4.png&f=1&nofb=1"
-                    reviewCount={16} />
-                </Grid> */}
-                <Grid item md={2}>
-                    <Product
-                    productId="12321341"
-                    name="Awesome Sweat Pants"
-                    price={20.00}
-                    avgRating={4}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fsweatpants-png-4.png&f=1&nofb=1"
-                    reviewCount={16} />
-                </Grid>
-                <Grid item md={2}>
-                    <Product
-                    productId="12321342"
-                    name="Awesome T-Shirt"
-                    price={15.00}
-                    avgRating={3}
-                    image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F04%2FT-Shirt-PNG-File.png&f=1&nofb=1"
-                    reviewCount={12}/>
-                </Grid>
-                <Grid item md={2}>
-                    <Product
-                    productId="12321343"
-                    name="Awesome Tennis Shoes"
-                    price={60.00}
-                    avgRating={4.5}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tension24.com.au%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2Fab687bdc54b01ee9b7e0fb822647627d%2Fc%2Fm%2Fcm7819_ftw_virtual_side-lateral_transparent.png&f=1&nofb=1"
-                    reviewCount={23}/>
-                </Grid>
-                <Grid item md={2}>
-                    <Product
-                    productId="12321344"
-                    name="Awesome Shorts"
-                    price={13.00}
-                    avgRating={4}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pro-amkits.co.uk%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F1%2Fimage%2F1200x1395%2F9df78eab33525d08d6e5fb8d27136e95%2F7%2F2%2F725881-010-pv_2.png&f=1&nofb=1"
-                    reviewCount={5}/>
-                </Grid>
-                <Grid item md={2}>
-                    <Product
-                    productId="12321345"
-                    name="Funky Socks"
-                    price={10.00}
-                    avgRating={5}
-                    image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdtmxsocks.cz%2Fwp-content%2Fuploads%2F2018%2F02%2FRocketSlider-background-2A.png&f=1&nofb=1"
-                    reviewCount={100}/>
-                </Grid>
-
+            
+                <div className="more_products_row">
+                    {products}
+                </div>
 
                 <Grid item md={1}>
                     <button class="nav-button" id="right-nav">
