8a830311a12c8847da3c4f95f6a45d6cb782bd91
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-04-20 16:45:41 -0400

    change component location

--- nile-frontend/src/components/ReviewTag/Tag.jsx
+++ nile-frontend/src/components/ReviewTag/Tag.jsx
@@ -1,33 +1,25 @@
 import "./Tag.css";
-import { useEffect, useState } from "react";
-import {getReviews} from '../../service/ReviewService';
-import Reviews from "../Reviews/Reviews";
+import React from 'react'
+ 
+//Find common phrases from the reviews
+//Fill this array with those phrases
+//filter by the phrases
 
 //hard coded phrases
-const tagArr = ["soft touch", "great length","easy to manage"];
-const reviewArr = ["This is soft to the touch", "This has a great length!","This is easy to manage!", "This is soft and has a great length!", "This is a very good value",
-"Amazing cost and very soft", "Really long and amazing. So easy to handle!"];
+const tagArr = ["soft", "length", "manageable"];
+// const reviewArr = ["This is soft to the touch", "This has a great length!","This is easy to manage!", "This is soft and has a great length!", "This is a very good value",
+// "Amazing cost and very soft", "Really long and amazing. So easy to handle!"];
 
-
+//potentially can leave this out
 function Tag (props)
 {
-    
-    const [reviews, setReviews]= useState(reviewArr);
-    const [filter, setFilter]= useState([]);
-
-
-
-
-
-    console.log(reviews)
-
-
-    //Find common phrases from the reviews
-    //Fill this array with those phrases
-    //filter by the phrases
+    console.log(props.reviews);
 
     const tagList = tagArr.map((phrase) =>
-    <button>{phrase}</button> );
+    <button 
+    value = {phrase}>
+        {phrase}
+    </button> );
 
 
     return (
@@ -39,5 +31,5 @@ function Tag (props)
     );
 }
 
-export default Tag;
 
+export default Tag;
