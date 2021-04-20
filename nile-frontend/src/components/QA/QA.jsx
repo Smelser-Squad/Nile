cbf5604311b6eae3ae437e13cfcf51ff84866ce8
Victor Barron <victorbarron@C02DK48MML7H.attlocal.net>
2021-04-20 09:23:11 -0400

    committing progress

--- nile-frontend/src/components/QA/QA.jsx
+++ nile-frontend/src/components/QA/QA.jsx
@@ -1,33 +1,77 @@
 import './QA.css';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function QuestionAnswer() {
+    const productId = 1;
+
+    useEffect(()=>{
+        getQuestions();
+    })
+
+    const getQuestions=async()=>{
+        await axios.get(`http://localhost:80/api/qa/questions/${productId}`)
+        .then(res=>{
+            const questions = res.data;
+            console.log(questions[0]);
+            });
+    }
 
     return(
         <div className="section">
             <h2>Customer questions & answers</h2>
             <input type="text" className="search" placeholder="Have a question? Search for answers"></input>
-            <div className="questions">
-                <div className="button">
-                    <img className="toggleVoteUp" src="https://cdn4.iconfinder.com/data/icons/neutro-award/32/upvote-512.png"></img><br></br>
+            <Grid container spacing={3}>
+                <Grid item xs={1}>
+                <img className="toggleVoteUp" src="https://cdn4.iconfinder.com/data/icons/neutro-award/32/upvote-512.png" onClick={upVote} ></img><br></br>
                     <div className="buttonText">
-                        45<br></br>
+                        22{getQuestions().questions[0].votes}<br></br>
                         votes<br></br>
                     </div>
-                    <img className="toggleVoteDown" src="https://cdn4.iconfinder.com/data/icons/neutro-award/32/downvote-512.png"></img>
+                    <img className="toggleVoteDown" src="https://cdn4.iconfinder.com/data/icons/neutro-award/32/downvote-512.png" onClick={downVote}></img>
+                </Grid>
+                <Grid item xs={6}>
+                <div className="queAndAns">
+                    <div className="left">
+                        Question: <br></br>
+                        Answer: 
+                    </div>
+                    <div className="right">
+                        Some question asked?!<br></br>
+                        Some answer given.. <br></br>
+                        by userOne on 04/19/2020<br></br>
+                        {3>1 ? <button className="borderlessButton" onClick={expandAnswer}> See more answers</button> : null}
+                    </div>
+                    <br></br>
                 </div>
-            </div>
+                </Grid>
+            </Grid>
         </div>
         
     );
-    // function expandAnswer() {
-        
-    //     }
-    // function viewAllQuestions(){
-
-    // }
-    // function search(){
-
-    // }
 }
+    function expandAnswer() {
+        console.log("expand");
+    }
+    function viewAllQuestions(){
+
+    }
+    function search(){
+
+    }
+    function upVote(){
+        console.log("upvote");
+    }
+    function downVote(){
+        console.log("downvote");
+    }
+    function postAnswer(){
+
+    }
+    function postQuestion(){
+
+    }
+
 
 export default QuestionAnswer;
