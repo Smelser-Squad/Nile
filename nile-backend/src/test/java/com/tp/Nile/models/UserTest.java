1a70b181e6d31fb1b790aa77f1ce8f457bbf1577
bdeyo28 <77344033+bdeyo28@users.noreply.github.com>
2021-04-21 15:29:45 -0400

    Review fixedbackendbuild (#141)
    * merging
    
    * merging
    
    * merging with master
    
    * merging with master
    
    * merging with master
    
    * jpa unit testing for review
    
    * committing requested changes
    
    * pushing changes
    
    * merging
    
    * pushing front end changes
    
    * committing changes
    
    * first local push
    
    * merging with master
    
    * pushing branch
    
    * first push to branch
    
    * finished most of back end testing for review
    
    * first local push
    
    * added backend components to user
    
    * first push to new branch
    
    * pushing changes
    
    * local push
    
    Co-authored-by: Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
    Co-authored-by: wcollins37 <collinswilliam122396@gmail.com>

---
+++ nile-backend/src/test/java/com/tp/Nile/models/UserTest.java
@@ -0,0 +1,86 @@
+package com.tp.Nile.models;
+
+
+import com.tp.Nile.repositories.ProductRepository;
+import static org.junit.jupiter.api.Assertions.*;
+import com.tp.Nile.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class UserTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @BeforeEach
+    public void setup() {
+        repo.deleteAll();
+
+        List<Cart> cartList = new ArrayList<>();
+        Cart newCart = new Cart();
+        newCart.setCartId(1);
+        newCart.setPurchaseDate(LocalDate.of(2020, 4, 21));
+        newCart.setStatus("Order pending");
+        cartList.add(newCart);
+
+        Set<Answer> answerSet = new HashSet<>();
+        Answer newAnswer = new Answer();
+        newAnswer.setAnswerId(1);
+        answerSet.add(newAnswer);
+
+        User setupUser =  new User();
+
+        setupUser.setUserId(1);
+        setupUser.setAnswers(answerSet);
+        setupUser.setCarts(cartList);
+
+    }
+
+    @Test
+    @Rollback(true)
+    public void testAddUser() {
+
+        List<Cart> newList = new ArrayList<>();
+        Cart cart = new Cart();
+        cart.setCartId(10);
+        cart.setPurchaseDate(LocalDate.of(2022, 4, 21));
+        cart.setStatus("Order pending 2");
+        newList.add(cart);
+
+        Set<Answer> newSet = new HashSet<>();
+        Answer answer = new Answer();
+        answer.setAnswerId(2);
+        newSet.add(answer);
+
+        User setupUser =  new User();
+
+        setupUser.setUserId(2);
+        setupUser.setAnswers(newSet);
+        setupUser.setCarts(newList);
+
+        User savedUser = repo.save(setupUser);
+
+        assertNotNull(savedUser);
+
+    }
+
+}
