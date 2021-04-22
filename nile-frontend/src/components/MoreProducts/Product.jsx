10dab97d02e9f3e233d0ef3dcc2946f0ad1b3b83
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-04-22 09:52:25 -0400

    Added proper routing to route to specific product page based on id.

--- nile-frontend/src/components/MoreProducts/Product.jsx
+++ nile-frontend/src/components/MoreProducts/Product.jsx
@@ -6,7 +6,7 @@ function PrimeLogo(props) {
     const primeEligible = props.primeEligible;
     if(primeEligible) {
         return (
-            <Link to="/singleProductListing">
+            <Link to={`/singleProductListing/${props.productId}`}>
                 <img id="prime-img" alt="prime" src="https://external-content.duckduckgo.com/iu/?u=https://curlydavenport.com/wp-content/uploads/2018/05/Amazon-Prime-Logo-Curly-D-Pink-Coco.png&f=1&nofb=1"></img>
             </Link>
         );
@@ -20,12 +20,12 @@ function Product({ productId, image, name, price, avgRating, reviewCount, primeE
     return (
         <div class="products-container">
             <div class="row">
-                <Link to="/singleProductListing">
+                <Link to={`/singleProductListing/${productId}`}>
                     <img class="image" id="product-image" src={image} alt="product_image"></img>
                 </Link>
             </div>
             <div class="row">
-                <Link to="/singleProductListing" style={{ textDecoration: 'none' }}>
+                <Link to={`/singleProductListing/${productId}`} style={{ textDecoration: 'none' }}>
                     <p id="product-name">{name}</p>
                 </Link>
             </div>
@@ -37,15 +37,15 @@ function Product({ productId, image, name, price, avgRating, reviewCount, primeE
                     activeColor="#FFA41C"
                     size={15}
                 />
-                <Link to="/singleProductListing" style={{ textDecoration: 'none' }}>
+                <Link to={`/singleProductListing/${productId}`} style={{ textDecoration: 'none' }}>
                     <p id="review-count">{reviewCount}</p>
                 </Link>
             </div>
             <div class="row">
-                <Link to="/singleProductListing" style={{ textDecoration: 'none' }}>
+                <Link to={`/singleProductListing/${productId}`} style={{ textDecoration: 'none' }}>
                     <p id="price-tag">${price}</p>
                 </Link>
-                <PrimeLogo primeEligible={primeEligible}/>
+                <PrimeLogo primeEligible={primeEligible} productId={productId}/>
             </div>
         </div>
     );
