9c473bac7e22d92038a79db5919357b140d52776
Jason-Williams-github <jwilliams@talentpath.com>
2021-04-22 13:30:53 -0400

    fixed tests

--- nile-backend/src/main/java/com/tp/Nile/repositories/ReviewRepository.java
+++ nile-backend/src/main/java/com/tp/Nile/repositories/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.tp.Nile.repositories;
 import com.tp.Nile.models.Review;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -14,4 +15,6 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findByUserUserId(Integer userId);
 
+    @Query("select p from Review p where p.product.productId=:productId")
+    List<Review> findByProductId(Integer productId);
 }
