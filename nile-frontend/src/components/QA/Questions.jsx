9f9ff74fb812fa98764239e9e06792026fb15860
Victor Barron <victorbarron@C02DK48MML7H.attlocal.net>
2021-04-28 14:53:21 -0400

    removed console.logs

--- nile-frontend/src/components/QA/Questions.jsx
+++ nile-frontend/src/components/QA/Questions.jsx
@@ -3,9 +3,7 @@ import './QA.jsx';
 import Question from "./SingleQuestion";
 
 export default function Questions(props) {
-        console.log(props);
         const {questions} = props;
-        console.log(questions);
         const quest = questions.map((question, index) => {
             return( <Question question={question} />)
         });
