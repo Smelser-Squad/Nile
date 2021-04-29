2a9c8192ecfe81f2cfec317ccee795e5c26d5101
YesratRahman <yr2818@vt.edu>
2021-04-29 13:09:41 -0400

    Stripe backend testing

--- nile-backend/src/main/java/com/tp/Nile/config/SecurityConfig.java
+++ nile-backend/src/main/java/com/tp/Nile/config/SecurityConfig.java
@@ -78,7 +78,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.jpg",
                         "/**/*.html",
                         "/**/*.css",
-                        "/**/*.js")
+                        "/**/*.js",
+                        "/h2-console/**")
                 .permitAll()
                 .antMatchers("/api/auth/**")
                 .permitAll()
