3449559bcb3ea7a75d193013f00212f8d35c5916
zmiles17 <zachariasmiles@gmail.com>
2021-04-30 13:36:25 -0400

    Update data-dev.sql

--- nile-backend/src/main/resources/data-dev.sql
+++ nile-backend/src/main/resources/data-dev.sql
@@ -1,5 +1,5 @@
-INSERT INTO "role"
-    SELECT r.id, r.role FROM (VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN')) as r(id, role)
+INSERT INTO "role"("name")
+    SELECT r.role FROM (VALUES ('ROLE_USER'), ('ROLE_ADMIN')) as r(role)
     WHERE NOT EXISTS(
         SELECT "name" FROM "role" WHERE "name" = 'ROLE_USER'
             OR "name" = 'ROLE_ADMIN'
