57315a9b878afb3648acc726fc5161386af4e71d
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-05-03 16:28:53 -0400

    adding useEffect

--- nile-frontend/src/components/Reviews/ShowAllReviews.js
+++ nile-frontend/src/components/Reviews/ShowAllReviews.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import  "./ShowAllReviews.css";
 import {useParams} from 'react-router'
 import {getProduct} from '../../service/ProductService';
@@ -10,64 +10,72 @@ import SingleReview from './SingleReview';
 function ShowAllReviews (){
 
     const[allReviews, setAllReviews] = useState([]);
+    const[filteredReviews, setFilteredReviews] = useState([]);
     const[mostPositive, setPositive]= useState();
     const[mostNegative, setNegative]=useState();
     const { productId } = useParams();
 
     const reviewsList =[];
-    // const positive=[];
-    // const negative=[];
 
-
-    if(allReviews.length===0)
+    useEffect(()=>
     {
-        getReviews(productId).then((list)=>{
-            list.map((summary)=>
-            reviewsList.push(summary));
-            const reviews = reviewsList.map((review)=>
-            <SingleReview
-            key={review.reviewId}
-            helpful={review.helpful}
-            rating={review.rating}
-            reviewDate={review.reviewDate}
-            title={review.title}
-            summary={review.summary}/>
-            );
-            const positive = reviewsList.filter(review => review.rating === 5).slice(0,1).map((review)=>
-            <SingleReview
-            key={review.reviewId}
-            helpful={review.helpful}
-            rating={review.rating}
-            reviewDate={review.reviewDate}
-            title={review.title}
-            summary={review.summary}/>
-            );
-            const negative = reviewsList.filter(review => review.rating === 1).slice(0,1).map((review)=>
-            <SingleReview
-            key={review.reviewId}
-            helpful={review.helpful}
-            rating={review.rating}
-            reviewDate={review.reviewDate}
-            title={review.title}
-            summary={review.summary}/>
-            );
-            setPositive(positive);
-            setNegative(negative);
-        
-            setAllReviews(reviews);
-        })
-    }
-    console.log(mostPositive)
-    console.log(mostNegative)
-
+        if(allReviews.length===0)
+        {
+            getReviews(productId).then((list)=>{
+                list.map((summary)=>
+                reviewsList.push(summary));
+                const reviews = reviewsList.map((review)=>
+                <SingleReview
+                key={review.reviewId}
+                helpful={review.helpful}
+                rating={review.rating}
+                reviewDate={review.reviewDate}
+                title={review.title}
+                summary={review.summary}/>
+                );
+                const positive = reviewsList.filter(review => review.rating === 5).slice(0,1).map((review)=>
+                <SingleReview
+                key={review.reviewId}
+                helpful={review.helpful}
+                rating={review.rating}
+                reviewDate={review.reviewDate}
+                title={review.title}
+                summary={review.summary}/>
+                );
+                const negative = reviewsList.filter(review => review.rating === 1).slice(0,1).map((review)=>
+                <SingleReview
+                key={review.reviewId}
+                helpful={review.helpful}
+                rating={review.rating}
+                reviewDate={review.reviewDate}
+                title={review.title}
+                summary={review.summary}/>
+                );
+                setPositive(positive);
+                setNegative(negative);
+            
+                setAllReviews(reviews);
+            })
+        }        
+    },[allReviews.length,productId,reviewsList,]);
+    
+ 
 
 
     return(
         <div class="all-reviews-container">
-            <h3>Top positive review:</h3>
-            {mostPositive}
-            <h3>Top critical review:</h3>
-            {mostNegative}
+            <hr></hr>
+            <div class="top-reviews">
+            <div class="positive-review">
+                <h3>Top positive review:</h3>
+                {mostPositive}
+            </div>
+            <div class="negative-review">            
+                <h3>Top critical review:</h3>
+                {mostNegative}
+            </div>
+            </div>
+            <hr></hr>
             {allReviews}
         </div>
     )
