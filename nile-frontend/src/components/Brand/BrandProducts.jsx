785cafaafff85e33bb4424e8e3d561895dffcbcb
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-05-03 09:45:31 -0400

    Adding filters

--- nile-frontend/src/components/Brand/BrandProducts.jsx
+++ nile-frontend/src/components/Brand/BrandProducts.jsx
@@ -18,7 +18,10 @@ function BrandProducts(){
             list.map((item) =>
                 ProductList.push(item)
             );
-            const cards = ProductList.map((product) =>
+           
+
+
+            let cards = ProductList.map((product) =>
             <div className="BrandProduct">
             <h1 id="name"  onClick={() => { window.location.href = `/singleProductListing/${product.productId}` }}>{product.name} - {product.photos[0].color} </h1>
             <img id="product-image" src={product.photos[0].imageSrc} alt=""></img>
@@ -40,7 +43,7 @@ function BrandProducts(){
             </div>
         </div>
             );
-            setProduct(cards);
+      setProduct(cards);
         }
         );
     }
@@ -66,16 +69,63 @@ function BrandProducts(){
         const avgRating = sum / product.reviews.length;
         return avgRating;
     }
-   
+   function compareValues(key,order='asc'){
+    return function innerSort(a,b){
+        if(!a.hasOwnProperty(key)|| !b.hasOwnProperty(key)) return 0;
+        const varA=(typeof a[key]==='string') ? a[key].toUpperCase():a[key];
+        const varB=(typeof b[key]==='string') ? b[key].toUpperCase():b[key];
+
+        let compare=0;
+        if(varA>varB){
+            compare=1;
+        } else if(varA < varB){
+            compare=-1;
+        }
+        return(
+            (order==='desc') ? (compare * -1) : compare
+        );
+
+    };
+   }
+   function sort(){
+    
+       console.log('hi');
+    let list2= ProductList.sort(compareValues('price'));
+     
+    // console.log(list2);
+       let cards = list2.map((product) =>
+            <div className="BrandProduct">
+            <h1 id="name"  onClick={() => { window.location.href = `/singleProductListing/${product.productId}` }}>{product.name} - {product.photos[0].color} </h1>
+            <img id="product-image" src={product.photos[0].imageSrc} alt=""></img>
+            <div className="BrandProduct_info">
+            <div class="row">
+            <ReactStars
+                    count={5}
+                    edit={false}
+                    isHalf={true}
+                    value={calcRating(product)}
+                    activeColor="#FFA41C"
+                    size={15}
+                />
+          
+            <p id="review-count">{product.reviews.length}</p>
+            </div>
+            <h2>${product.price}</h2>
+            <PrimeLogo primeEligible={product.primeEligible} productId={product.productId} />
+            </div>
+        </div>
+            );
+            setProduct(cards);
+   }
 return(
     <div>
         <h1>{Product.length} results for "{brand}"</h1>
-        {/* <select id="sort">
+        <select id="sort">
         <option selected disabled hidden >Sort By</option>
         <option>Price:High to Low</option>
-        <option>Price:Low to High</option>
-        </select> */}
-        
+        <option> Price:Low to High</option>
+        </select>
+        <button type="button" onClick={()=>sort()}>Search</button>
     {Product}
     </div>
 )
