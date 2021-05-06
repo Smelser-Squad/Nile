c096d51da3a556e558d4d70d9f7c7751e6ea0655
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-05-06 14:40:02 -0400

    first push to local

---
+++ nile-backend/src/test/java/com/tp/Nile/seleniumTests/registerTest.java
@@ -0,0 +1,39 @@
+package com.tp.Nile.seleniumTests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class registerTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/brendandeyo/Desktop/Nile/nile-backend/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:3000/signup");
+
+        WebElement fullnameElement = driver.findElement(By.id("fname"));
+        WebElement usernameElement = driver.findElement(By.id("username"));
+        WebElement emailElement = driver.findElement(By.id("email"));
+        WebElement passwordElement = driver.findElement(By.id("password"));
+
+        fullnameElement.sendKeys("Brendan Deyo");
+        usernameElement.sendKeys("Bdeyo28");
+        emailElement.sendKeys("bdeyo1@talentpath.com");
+        passwordElement.sendKeys("Password123");
+
+//        fullnameElement.submit();
+//        usernameElement.submit();
+//        emailElement.submit();
+//        passwordElement.submit();
+
+
+
+    }
+
+}
