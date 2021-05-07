cabaffbefd0ab0c498449da28954618c46f99219
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-05-07 11:05:40 -0400

    Filter Works

--- nile-frontend/src/components/Brand/AllBrandProducts.jsx
+++ nile-frontend/src/components/Brand/AllBrandProducts.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import { getBrandProducts } from '../../service/ProductService';
 import Pagination from "../../common/Pagination";
 import BrandProduct from './BrandProduct';
-
+import ReactStars from "react-rating-stars-component";
 
 
 function AllBrandProducts(){
@@ -60,7 +60,21 @@ const currentProducts = allProducts.slice(indexOfFirstProduct, indexOfLastProduc
         return avgRating;
     }
     
-   
+    function PrimeLogo(props) {
+        
+        const primeEligible = props.primeEligible;
+        if (primeEligible) {
+            return (
+                <img id="prime-img" alt="prime" src="https://external-content.duckduckgo.com/iu/?u=https://curlydavenport.com/wp-content/uploads/2018/05/Amazon-Prime-Logo-Curly-D-Pink-Coco.png&f=1&nofb=1"
+                
+                ></img>
+    
+            );
+        }
+        else {
+            return <div></div>
+        }
+    }
    function Sort(){
 
     var selected=document.getElementById("sort").value
@@ -83,22 +97,33 @@ const currentProducts = allProducts.slice(indexOfFirstProduct, indexOfLastProduc
         }
 
 
-    //   const filtered = ProductList.map((product)=>
-    //         <BrandProduct
-    //         key={product.productId}
-    //         productId={product.productId}
-    //         name={product.name}
-    //         price={product.price}
-    //         rating={calcRating(product)}
-    //         image={product.photos[0].imageSrc}
-    //         color={product.photos[0].color}
-    //         primeEligible={product.primeEligible}
-    //         reviews={product.reviews.length}
-    //         />
-    //         );
-    //         setallProducts(filtered);
-    //         setSortedProducts(filtered)
-    }
+      const filtered = ProductList.map((product)=>
+
+      <div className="BrandProduct">
+      <h1 id="name"  onClick={() => { window.location.href = `/singleProductListing/${product.productId}` }}>{product.name} - {product.color} </h1>
+      <img id="product-image" src={product.image} alt="" onClick={() => { window.location.href = `/singleProductListing/${product.productId}` }}></img>
+       <div className="BrandProduct_info">
+       <div class="row">
+      <ReactStars
+              count={5}
+              edit={false}
+               isHalf={true}
+              value={product.rating}
+              activeColor="#FFA41C"
+              size={15}
+           />
+    
+
+      <p id="review-count">{product.reviews}</p>
+       </div>
+      <h2>${product.price}</h2>
+       <PrimeLogo primeEligible={product.primeEligible} productId={product.productId} />
+       </div>
+       </div>
+            );
+            setallProducts(filtered);
+           
+     }
     
    
    
